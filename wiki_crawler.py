80ce73f741fb478cc05594ca529fc700ab5c1b36
b-insh <bethanyhyland29@gmail.com>
2017-02-13 12:03:25 -0500

    Implement get random formatted urls

--- wiki_crawler.py
+++ wiki_crawler.py
@@ -7,5 +7,26 @@ class WikiCrawler(object):
     """
 
     def __init__(self):
+        self.philosophy = '/wiki/Philosophy'
 
-    def begin_crawl(self):
+        self.checkable_urls = self._get_urls()
+
+        self.seen_urls = {}
+        self.path = {}
+
+    def _get_urls(self):
+        payload = {
+            "action": "query",
+            "format": "json",
+            "list": "random",
+            "rnnamespace": "0",
+            "rnlimit": "5"
+        }
+
+        r = requests.get('http://en.wikipedia.org/w/api.php?', params=payload)
+        pages = r.json()['query']['random']
+        return (['/wiki/' + page['title'].replace(" ", "_") for page in pages])
+
+
+crawler = WikiCrawler()
+# crawler._get_urls()
