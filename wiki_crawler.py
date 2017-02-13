d38783331e24fd195eea056afdf4ae2ea7d940fb
b-insh <bethanyhyland29@gmail.com>
2017-02-13 11:37:54 -0500

    First commit

---
+++ wiki_crawler.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+class WikiCrawler(object):
+    """
+    Wikipedia crawler that an article and follows the first link on the main body of the article on each subsequent page until reaching the Philosophy page
+    """
+
+    def __init__(self):
+
+    def begin_crawl(self):
